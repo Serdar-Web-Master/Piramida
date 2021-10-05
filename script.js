c3c42f343161b80b922d46399f519d1a4d4e8c59
Serdar-Web-Master <serdarcholukov@yandex.com>
2021-10-05 18:25:36 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let start = document.querySelector(".start");
+
+let line1 = document.querySelector(".line1");
+
+// document.querySelector(".start").onclick = () => {
+//   line1.style.marginTop = "-500px";
+//   console.log("rabotaet");
+// };
+
+start.onclick = function f1() {
+  line1.style.marginTop = "-500px";
+  console.log("rabotaet");
+};
